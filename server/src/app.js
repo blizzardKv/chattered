dbe86b6416ff72bbb912245423818b4489f23f3b
Konstantin Pavlyuts <makemewanna@yandex.ru>
2022-03-19 21:11:47 +0300

    server setup, #1

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+const mongodb_conn_module = require('./mongodbConnModule');
+var db = mongodb_conn_module.connect();
+
+app.listen(process.env.PORT || 8081)
